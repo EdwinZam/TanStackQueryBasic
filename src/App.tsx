9878a178d94c49c90e786d69cb0b7a93d6ed244c
Edwin Zambrano <eaespinosa9312@hotmail.com>
2023-06-06 16:02:56 -0500

    Random number React Query Basic

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { useRandom } from './useRandom'
+
+export const App = () => {
+
+  const query = useRandom();
+
+  return (
+    <>
+ 
+      <div className="card">
+        {/* {isLoading && (<h2>Cargando ...</h2>)}
+        <h2>Número aleatorio: {number} </h2> */}
+        { 
+          query.isFetching 
+            ? (<h2>Cargando ...</h2>) 
+            : (<h2>Número aleatorio: {query.data} </h2>) 
+        }
+        {
+          !query.isLoading && query.isError && (<h3> {`${query.error}`} </h3>)
+        }
+
+        <button onClick={ ()=> query.refetch()} disabled={query.isFetching}>
+          {
+            query.isFetching ? '...' : 'Nuevo número'
+          }
+        </button>
+      </div>
+
+    </>
+  )
+}
